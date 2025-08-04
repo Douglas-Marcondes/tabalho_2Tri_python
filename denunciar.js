b70c8460f7e0058457d15f0bd7b2cb1b79c9aa0e
Douglas-Marcondes <159036433+Douglas-Marcondes@users.noreply.github.com>
2025-08-04 00:02:12 -0300

    Update denunciar.js

--- denunciar.js
+++ denunciar.js
@@ -1,1 +1,13 @@
+const tema = localStorage.getItem('tema') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
 
+if (tema === 'dark') {
+    document.body.classList.add('dark-mode');
+    document.getElementById('botao-tema-escuro').innerHTML = '<i class="fas fa-sun"></i>';
+}
+
+document.getElementById('botao-tema-escuro').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    const temaNovo = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+    localStorage.setItem('tema', temaNovo);
+    this.innerHTML = temaNovo === 'dark' ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
+});
